bd84a5f1c67dd267f6fef1f5709a35a84abb71e0
Samantha Xavia <105689964+SamXavia@users.noreply.github.com>
2022-10-20 14:00:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var example = ['world', 'web', 'video', 'community', 'service'];
+var landingswitchtext = ['world', 'web', 'video', 'community', 'service'];
 
 textSequence(0);
 function textSequence(i) {
 
             if (example.length > i) {
                     setTimeout(function() {
-                        document.getElementById("landing-text-switch").innerHTML = example[i];
+                        document.getElementById("landing-text-switch").innerHTML = landingswitchtext [i];
                         textSequence(++i);
                     }, 2500); // 2.5s (in milliseconds)
 
-                } else if (example.length == i) { // Loop
+                } else if (landingswitchtext.length == i) { // Loop
                     textSequence(0);
                 }
             }
