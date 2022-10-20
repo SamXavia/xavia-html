60d6b4d49a1abb0dce7514d8d4ff20288ca66165
Samantha Xavia <105689964+SamXavia@users.noreply.github.com>
2022-10-20 13:30:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+var example = ['better world', 'better web', 'better video', 'better community'];
 
+            textSequence(0);
+            function textSequence(i) {
+
+                if (example.length > i) {
+                    setTimeout(function() {
+                        document.getElementById("sequence").innerHTML = example[i];
+                        textSequence(++i);
+                    }, 1000); // 1 second (in milliseconds)
+
+                } else if (example.length == i) { // Loop
+                    textSequence(0);
+                }
+            }
