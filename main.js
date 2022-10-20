b412607c780b9afc39273193a78737d17f11d73e
Samantha Xavia <105689964+SamXavia@users.noreply.github.com>
2022-10-20 10:49:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-
+function isEmail(mail) {
+ let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+ return regex.test(String(mail).toLowerCase());
+}
